b1bd175a80f0603b8f0d5ee7bce829359bd03fc2
Shruthi <shruthi.m@iiht.com>
2020-06-02 20:15:18 +0530

    first commit

---
+++ app/scripts/app.js
@@ -0,0 +1,30 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name todoApp
+ * @description
+ * # todoApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('todoApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
